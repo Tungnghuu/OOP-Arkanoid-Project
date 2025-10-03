625ffd0a3fdf4303183c631e3ec8f60a0997f85d
Ducquangbill <ducquangnaoh@gmail.com>
2025-10-03 09:25:48 +0700

    feat: implement main game window and input handling

---
+++ interface/src/GamePanel.java
@@ -0,0 +1,52 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
+public class GamePanel  extends JPanel implements Runnable {
+    final int originalTileSize = 16;
+    final int scale = 3;
+    final int tileSize = originalTileSize * scale;
+    int maxScreenCol = 16;
+    int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    InputHandler inputHandler = new InputHandler();
+    Thread gameThread;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(inputHandler);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while (gameThread != null) {
+            update();
+            repaint();
+        }
+    }
+
+    public void update() {
+        
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.white);
+        g2.fillRect(0, 0, tileSize * 3, tileSize / 2);
+        g2.dispose();
+        
+    }
+}
